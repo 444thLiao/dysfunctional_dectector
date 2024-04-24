27c689b557733baea3e40efac6ace54f7262b1cf
Thliao <l0404th@gmail.com>
2024-04-24 10:11:48 +0800

    modify s1.py

--- raw_workflow/s1.annotation.py
+++ raw_workflow/s1.annotation.py
@@ -13,19 +13,13 @@ from dysfunctional_dectector.src.utilities.tk import check
 import logging
 import click
 from sys import exit
-from pathlib import Path
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import os
 from subprocess import CalledProcessError
 from multiprocessing import Process
-## dynamic input
-infaa = ''
-odir = './'
-gbk = ''
-in_file=""
-dry_run = True
+from collections import defaultdict
 #enable logging
 import logging
 logger = logging.getLogger('dysfunction_logger')
@@ -48,142 +42,143 @@ diamond_path = '/home-db/pub/protein_db/nr/v20230523/diamond_index/diamond'
 diamond_db = "/home-db/pub/protein_db/nr/v20230523/diamond_index/nr.dmnd"
 pseudofinder_exe = 'python3 /mnt/storage3/yfdai/download/pseudofinder/pseudofinder-master/pseudofinder.py'
 ipr_exe = '/mnt/storage3/yfdai/download/interproscan/interproscan-5.67-99.0/interproscan.sh'
-cpu = 5
-#命令行工具
-@click.command()
-@click.option('-fi',"--file_input",type = str, nargs = 2 ,required = True, prompt = "Enter the input file name", help = "Please input faa or gbk file you want to analyze")
-@click.option('-o',"--folder_output",type = str, nargs = 1 ,required = True, prompt = "Enter the output folder name", help = "Please input path of folder you want to analyze")
-def cli(file_input,folder_output):
+num_cpu = 5
+
+suffix_gbk = 'gbk'
+suffix_protein = 'faa'
+
+
+def gbk2faa(gbk):
+    #gbk_seq = []
+    records = list(SeqIO.parse(gbk, format="genbank"))
+    faa_list = []
+    for contig in records:
+        prefix = contig.name if contig.name else ""
+        idx = 1
+        for fea in contig.features:
+            if fea.type != "CDS":
+                continue
+            if prefix:
+                seq_name = f"{prefix}_" + "{:05d}".format(idx)
+            else:
+                seq_name = fea.qualifiers.get("locus_tag", [""])[0]
+                if not seq_name:
+                    seq_name = fea.qualifiers.get("protein_id", [""])[0]
+            
+                _seq = Seq(fea.qualifiers.get("translation",[''])[0])
+                new_record = SeqRecord(
+                    seq=_seq,
+                    id=seq_name,
+                    name=fea.qualifiers.get("gene", [""])[0],
+                    description=fea.qualifiers.get("product",[""])[0],
+                    )
+                faa_list.append(new_record)
+                idx += 1            
+    return faa_list
+
+def processing_IO(file_input,folder_output):
+    in_files = defaultdict(dict)
     odir = folder_output
     temp_dir = odir + "/temp_file"
     os.makedirs(temp_dir,exist_ok=True)
                 
-#check the consistence of the input file
-    gbk_seq=[]
-    faa_seq=[]
+    #check the consistence of the input file
+    file_input = [realpath(_) for _ in file_input]
+
+    gbk_seq = defaultdict(list)
+    faa_seq = defaultdict(list)
     for file in file_input:
         extens = basename(file).split(".")[-1]
-        if extens == "gbk":
+        genomename = basename(file).replace('.'+extens,'')
+        if extens == suffix_gbk:
             gbk = file
-            for record in SeqIO.parse(gbk, "genbank"):
-                for feature in record.features:
-                    if feature.type == "CDS":
-                        aa_seq = feature.qualifiers.get("translation", [""])[0]
-                        gbk_seq.append(aa_seq)
-                    else:
-                        pass
-            gbk_seq.sort(key=len)
-        elif extens == "faa":
+            gbk_seq[genomename] = gbk2faa(gbk)
+            in_files[genomename]['gbk'] = gbk
+        elif extens == suffix_protein:
             infaa = file
-            with open(infaa, "r") as file:
-                records = list(SeqIO.parse(file, "fasta"))
-                for record in records:
-                    sequence = record.seq
-                    faa_seq.append(sequence)
-            faa_seq.sort(key=len)
+            records = list(SeqIO.parse(file, "fasta"))
+            for record in records:
+                sequence = record.seq
+                faa_seq.append(sequence)
+            faa_seq[genomename] = gbk2faa(gbk)
         else:
             logging.error(f"The format of {file_input} is incorrect")
             print(f"The format of {file_input} is incorrect")
             exit()
     if gbk_seq == faa_seq:
-        in_file = infaa
+        in_files[genomename]['faa'] = infaa
         logger.debug("The faa and gbk file are consistent.Input faa file will be used.")
     else:
-        filename = gbk.split("/")[-1].split(".")[0]+ "_gbk.faa"
+        filename = genomename+ "_gbk.faa"
         faafgbk = os.path.join(temp_dir,filename)
-        records = list(SeqIO.parse(gbk, format="genbank"))
-        faa_list = []
-        for contig in records:
-            prefix = contig.name if contig.name else ""
-            idx = 1
-            for fea in contig.features:
-                if prefix:
-                    seq_name = f"{prefix}_" + "{:05d}".format(idx)
-                else:
-                    seq_name = fea.qualifiers.get("locus_tag", [""])[0]
-                    if not seq_name:
-                        seq_name = fea.qualifiers.get("protein_id", [""])[0]
-                if fea.type == "CDS":
-                    _seq = Seq(fea.qualifiers.get("translation",[''])[0])
-                    new_record = SeqRecord(
-                        seq=_seq,
-                        id=seq_name,
-                        name=fea.qualifiers.get("gene", [""])[0],
-                        description=fea.qualifiers.get("product",[""])[0],
-                    )
-                    faa_list.append(new_record)
-                    idx += 1
         if not exists(os.path.dirname(infaa)):
             os.makedirs(os.path.dirname(infaa))
         with open(faafgbk, "w") as f1:
-            SeqIO.write(faa_list, f1, "fasta")
-        logger.debug("Gbk file have been converted to faa file.")
-        in_file = faafgbk
+            SeqIO.write(gbk_seq[genomename], f1, "fasta")
+        logger.debug(f"Gbk file have been converted to faa file (see {filename})")
+        in_files[genomename]['faa'] = faafgbk
         logger.debug("The aa and gbk file are not consistent.The faa file converted from input gbk file will be used.")
     logger.debug("Check for the consistence has done.")
-## OUTPUT 
-    genome = basename(in_file).rpartition('.')[0]
-    kegg_oname = os.path.join(odir,'KOFAMSCAN',genome+'.kofamout')
-    pseudo_oname = os.path.join(odir,'pseudofinder',genome)
-    finalname = os.path.join(pseudo_oname,f'{genome}_nr_pseudos.gff')
-    ipr_oname = os.path.join(odir,'ipr',genome)
-######### Run kofamscan
-    ogdir = os.path.join(odir,'KOFAMSCAN')
-    os.makedirs(ogdir,exist_ok=True)
-    kofams = f"{KOFAMSCAN_exe} -p {KOFAMSCAN_profiles} -k {KOFAMSCAN_ko_list} --tmp-dir .{genome}_tmp -o {kegg_oname} -f mapper-one-line --no-report-unannotated {in_file} "
-    kofams += f' && rm -rf .{genome}_tmp'
-    check(kegg_oname,kofams,'pseudofinder',dry_run=dry_run)
-    logger.debug("Start KoFamscan")
-    try:
-        Kofampros = Process(target=os.system, args=(kofams,))
-        Kofampros.start()
-        Kofampros.join()
-        logger.debug("The KofanScan has done")
-    except CalledProcessError as err:
-        logger.error("The KofamScan fail to finish.")
-        exit()
-######### Select file for interproscan
-    file_inipr = f"{temp_dir}/{genome}.faa"
-    id2p = []
-    with open(kegg_oname, "r") as file:
-        for line in file:
-            module = []
-            line = line.strip()
-            module = line.split("\t")
-            id2p.append(module[0])
-    faa2i = []
-    records = list(SeqIO.parse(in_file, format="fasta"))
-    for record in records:
-        sequence_id = record.id
-        if sequence_id not in id2p:
-            faa2i.append(record)           
-    with open(file_inipr, "w") as i:
-        SeqIO.write(faa2i, i,"fasta")
-    logger.debug("Files for interproscan and pseudofinder are ready")
-######### Run interpro
-    iprcmd = f"""mkdir -p {ipr_oname} && export LD_LIBRARY_PATH='' && {ipr_exe} -i {file_inipr} -d {ipr_oname} -cpu 12 -iprlookup -appl CDD,Pfam"""
-    check(ipr_oname,iprcmd,'interpro')
-    logger.debug("Interproscan start")
-    try:
-        iprpros = Process(target=os.system,args=(iprcmd,))
-        iprpros.start()
-        iprpros.join()
-        logger.debug("The interproscan has done")
-    except CalledProcessError as err:
-        logger.error("The interproscan fail to finish.")
-        exit()
-######### Run pseudofinder   
-    pseudocmd = f"mkdir -p {pseudo_oname} ; {pseudofinder_exe} annotate -db {diamond_db}  -g {gbk} -t 12 -skpdb -op {pseudo_oname}/{genome}_nr -di -l 0.8 --compliant --diamond_path {diamond_path}"
-    check(finalname,pseudocmd,'pseudofinder',dry_run=dry_run)
-    logger.debug("Pseudofinder start")
-    try:
-        psdpros = Process(target=os.system,args=(pseudocmd,))
-        psdpros.start()
-        psdpros.join()
-        logger.debug("The pseudofinder has done")
-    except CalledProcessError as err:
-        logger.error("The pseudofinder fail to finish.")
-        exit()
+    
+    return in_files,odir,temp_dir
+
+# parse args
+@click.command()
+@click.option('-fi',"--file_input",type = str, nargs = 2 ,required = True, prompt = "Enter the input file name", help = "Please input faa or gbk file you want to analyze")
+@click.option('-o',"--folder_output",type = str, nargs = 1 ,required = True, prompt = "Enter the output folder name", help = "Please input path of folder you want to analyze")
+@click.option("-d","dry_run",help="Generate command only.",default=False,required=False,is_flag=True,)
+def cli(file_input,folder_output,dry_run):
+    in_files,odir,temp_dir = processing_IO(file_input,folder_output,dry_run)
+    for genomename,info in in_files.items():
+        ingbk = info['gbk']
+        infaa = info['faa']
+        ## OUTPUT 
+        kegg_oname = os.path.join(odir,'KOFAMSCAN',genomename+'.kofamout')
+        pseudo_oname = os.path.join(odir,'pseudofinder',genomename)
+        finalname = os.path.join(pseudo_oname,f'{genomename}_nr_pseudos.gff')
+        ipr_oname = os.path.join(odir,'ipr',genomename)
+        ######### Run kofamscan
+        ogdir = os.path.join(odir,'KOFAMSCAN')
+        os.makedirs(ogdir,exist_ok=True)
+        kofams = f"{KOFAMSCAN_exe} -p {KOFAMSCAN_profiles} -k {KOFAMSCAN_ko_list} --tmp-dir {temp_dir}/{genomename}_kofamscan --cpu {num_cpu} -o {kegg_oname} -f mapper-one-line --no-report-unannotated {infaa} "
+        kofams += f' && rm -rf {temp_dir}/{genomename}_kofamscan'
+        check(kegg_oname,kofams,'pseudofinder',dry_run=dry_run)
+        logger.debug("Start KoFamscan")
+        try:
+            Kofampros = Process(target=os.system, args=(kofams,))
+            Kofampros.start()
+            Kofampros.join()
+            logger.debug("The KofanScan has done")
+        except CalledProcessError as err:
+            logger.error(f"The KofamScan fail to finish due to {err}")
+            exit()
+        logger.debug("Files for interproscan and pseudofinder are ready")
+        ######### Run interpro
+        iprcmd = f"""mkdir -p {ipr_oname} && export LD_LIBRARY_PATH='' && {ipr_exe} -i {infaa} -d {ipr_oname} -cpu {num_cpu} -iprlookup -appl CDD,Pfam"""
+        check(ipr_oname,iprcmd,'interpro')
+        logger.debug("Interproscan start")
+        try:
+            iprpros = Process(target=os.system,args=(iprcmd,))
+            iprpros.start()
+            iprpros.join()
+            logger.debug("The interproscan has done")
+        except CalledProcessError as err:
+            logger.error(f"The interproscan fail to finish due to {err}")
+            exit()
+        ######### Run pseudofinder   
+        pseudocmd = f"mkdir -p {pseudo_oname} ; {pseudofinder_exe} annotate -db {diamond_db}  -g {ingbk} -t {num_cpu} -skpdb -op {pseudo_oname}/{genomename}_nr -di -l 0.8 --compliant --diamond_path {diamond_path}"
+        check(finalname,pseudocmd,'pseudofinder',dry_run=dry_run)
+        logger.debug("Pseudofinder start")
+        try:
+            psdpros = Process(target=os.system,args=(pseudocmd,))
+            psdpros.start()
+            psdpros.join()
+            logger.debug("The pseudofinder has done")
+        except CalledProcessError as err:
+            logger.error(f"The pseudofinder fail to finish due to {err}")
+            exit()
+
+
 ######### RUN
 if __name__ == '__main__':
     cli()
