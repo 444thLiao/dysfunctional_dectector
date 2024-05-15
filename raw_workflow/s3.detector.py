58cad9a1479088fb7117a37c90965b54fe66a5bb
Thliao <l0404th@gmail.com>
2024-05-15 14:47:04 +0800

    fix s3

--- raw_workflow/s3.detector.py
+++ raw_workflow/s3.detector.py
@@ -67,20 +67,6 @@ def parse_name(row):
         mid = name.split(' ')[0]
         return mid
 
-
-def load_pickle(dictionary_location):
-    pickle_in = open(dictionary_location,"rb")
-    dictionary = pickle.load(pickle_in)
-    return dictionary
-
-def read_module2ko():
-    data_folder = join(dirname(__file__),'data')
-    # Import all modules from dictionaries
-    regular_modules = load_pickle(data_folder / "01.KEGG_Regular_Module_Information.pickle")
-    bifurcation_modules = load_pickle(data_folder / "02.KEGG_Bifurcating_Module_Information.pickle")
-    structural_modules = load_pickle(data_folder / "03.KEGG_Structural_Module_Information.pickle")
-    return regular_modules,bifurcation_modules,structural_modules
-
 level2depth = {'A':4,'B':3,'C':2,'D':1}
 def parse_brite_file(brite_file):
     basic_tre = Tree()
@@ -116,37 +102,32 @@ def parse_brite_file(brite_file):
 module_brite = parse_brite_file(brite_file)
 
 
-import numpy as np
-def map_pseudo(x):
-    if str(x)=='nan':
-        return np.nan
-    else:
-        if all([l in locus_is_pseudo for l in x.split(',')]) :
-            return np.nan
-        else:
-            return ','.join([l for l in x.split(',') if l not in locus_is_pseudo])
-gnano_kegg = kegg_df.applymap(map_pseudo)
-#####
-gid2withpseudo_kos = {}
-for gid,row in kegg_df.iterrows():
-    gid2withpseudo_kos[gid] = list(row.index[~row.isna()])
+def load_pickle(dictionary_location):
+    pickle_in = open(dictionary_location,"rb")
+    dictionary = pickle.load(pickle_in)
+    return dictionary
 
-gid2kos = {}
-for gid,row in gnano_kegg.iterrows():
-    gid2kos[gid] = list(row.index[~row.isna()])
+def read_module2ko():
+    data_folder = join(dirname(__file__),'data')
+    # Import all modules from dictionaries
+    regular_modules = load_pickle(data_folder / "01.KEGG_Regular_Module_Information.pickle")
+    bifurcation_modules = load_pickle(data_folder / "02.KEGG_Bifurcating_Module_Information.pickle")
+    structural_modules = load_pickle(data_folder / "03.KEGG_Structural_Module_Information.pickle")
+    return regular_modules,bifurcation_modules,structural_modules
 
 
-
-
-def main(odir,gid):
+def main(odir,gid,with_unannotated=False):
     refined_ko_infodf_path = f'{odir}/{gid}_refined_ko_info.tsv'
-    refined_ko_bindf_path = f'{odir}/{gid}_refined_ko_bin.tsv'
     ko_infodf = pd.read_csv(refined_ko_infodf_path,sep='\t',index_col=0)
     subko_df = ko_infodf.loc[ko_infodf[gid]!='no KEGG-annotated',:]
 
+    regular_modules,bifurcation_modules,structural_modules = read_module2ko()
+
     mentioned_kos = list(subko_df.index)
     related_modules = list(set([m for ko in mentioned_kos for m in ko2module.get(ko,'').split(';')]))
-
+    
+    for m in related_modules.items():
+        steps_ko = regular_modules[m]
 
 
 ######### RUN
